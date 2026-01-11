84e8dd05d0abc532197888b2720c53899b7c381a
fossilkeeper24 <babuli.rainbows@gmail.com>
2026-01-11 22:30:10 +0000

    upload kmk files

---
+++ kb60 - firmware/main.py
@@ -0,0 +1,32 @@
+print("Starting")
+
+import board
+
+from kmk.kmk_keyboard import KMKKeyboard
+from kmk.keys import KC
+from kmk.scanners import DiodeOrientation
+
+keyboard = KMKKeyboard()
+
+keyboard.col_pins = (board.GP0, board.GP1, board.GP2, board.GP3, board.GP4, board.GP5, board.GP6, board.GP7, board.GP8, board.GP9, board.GP10, board.GP11, board.GP12, board.GP13, board.GP14,)
+keyboard.row_pins = (board.GP15, board.GP16, board.GP17, board.GP18, board.GP19,)
+keyboard.diode_orientation = DiodeOrientation.COL2ROW
+
+keyboard.keymap = [
+    [KC.GESC, KC.1,    KC.2,    KC.3,    KC.4,    KC.5,    KC.6,    KC.7,    KC.8,    KC.9,    KC.0,    KC.MINS, KC.EQL,  KC.BSPC, KC.GRV,]
+        [KC.TAB,  KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,    KC.Y,    KC.U,    KC.I,    KC.O,    KC.P,    KC.LBRC, KC.RBRC, KC.NUHS, KC.NO,]
+        [KC.CAPS, KC.A,    KC.S,    KC.D,    KC.F,    KC.G,    KC.H,    KC.J,    KC.K,    KC.L,    KC.SCLN, KC.QUOT, KC.NUHS, KC.ENT,]
+        [KC.LSFT, KC.NUBS, KC.Z,    KC.X,    KC.C,    KC.V,    KC.B,    KC.N,    KC.M,    KC.COMM, KC.DOT,  KC.RSFT, KC.UP,   KC.SLSH, KC.PSCR,]
+        [KC.LCTL, KC.LGUI, KC.LALT, KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.SPC,  KC.NO,   KC.RALT, KC.RGUI, KC.LEFT, KC.DOWN, KC.RGHT]
+]
+
+if __name__ == '__main__':
+    keyboard.go()
+
+'''  [KC.ESC, KC.1,    KC.2,    KC.3,    KC.4,    KC.5,    KC.6,    KC.7,    KC.8,    KC.9,    KC.0,    KC.MINS, KC.EQL,  KC.BSPC, KC.GRV,]
+        [KC.TAB,  KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,    KC.Y,    KC.U,    KC.I,    KC.O,    KC.P,    KC.LBRC, KC.RBRC, KC.NUHS, KC.NO,]
+        [KC.CAPS, KC.A,    KC.S,    KC.D,    KC.F,    KC.G,    KC.H,    KC.J,    KC.K,    KC.L,    KC.SCLN, KC.QUOT, KC.NUHS, KC.ENT,]
+        [KC.LSFT, KC.NUBS, KC.Z,    KC.X,    KC.C,    KC.V,    KC.B,    KC.N,    KC.M,    KC.COMM, KC.DOT,  KC.RSFT, KC.UP,   KC.SLSH, KC.PSCR,]
+        [KC.LCTL, KC.LGUI, KC.LALT, KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.SPC,  KC.NO,   KC.RALT, KC.RGUI, KC.LEFT, KC.DOWN, KC.RGHT]
+
+'''
